ff12c1b04a901c6c0db53ed429190d0f4b40c7d8
kingduy119 <duy.nguyenhoang4@gameloft.com>
2019-12-19 17:27:25 +0700

    + Implement to test Socket.io done

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 1000;
 
 const server = http.createServer(app);
+const client = require('socket.io')(server).sockets;
+
+const schemaChat = mongoose.Schema({
+    name: {type: String},
+    message: {type: String}
+});
 
 // Mongoose connection:
 // ===========================================================================
@@ -22,9 +28,55 @@ const uri = `mongodb+srv://${
 mongoose
     .connect(uri, options)
     .then(() => {
-        console.log('Successfully connected to server mongodb atlas');
         mongoose.Promise = global.Promise;
+
+        client.on('connection', async (socket) => {
+            let ChatModel = mongoose.model('chat', schemaChat);
+
+            // Create func to send status
+            let sendStatus = (s) => {
+                socket.emit('status', s);
+            };
+
+            // Get chats from mongo collection
+            const res = await ChatModel.find({});
+            console.log(res);
+            socket.emit('output', res);// Emit input events
+            
+
+            // Handle input events
+            socket.on('input', (data) => {
+                let name = data.name;
+                let message = data.message;
+
+                // Check for name and message
+                if(name == '' || message == '') {
+                    sendStatus('Please enter a name and message');
+                } else {
+                    // Insert message
+                    ChatModel.create({name: name, message: message}, () => {
+                        socket.emit('output', [data]);
+
+                        // Send status object
+                        sendStatus({
+                            message: 'Message sent',
+                            clear: true
+                        });
+                    });
+                }
+            });
+
+            // Handle clear
+            socket.on('clear', (data) => {
+                // Remove all chats from collection
+                ChatModel.deleteMany({}, () => {
+                    socket.emit('cleared');
+                });
+            });
+        }); // Connect to socket.io
+
         server.listen(port);
+        console.log('Successfully connected to server mongodb atlas');
     })
     .catch(err => {
         console.log(err);
