3e32886a3165c298f3ebc649cfa1692f16666734
kingduy119 <hoangduy1192@gmail.com>
2020-02-11 10:59:47 +0700

    ! Update Navigation - 11/2: 11:00 AM

--- client/src/components/navbar/Navigation.js
+++ client/src/components/navbar/Navigation.js
@@ -6,7 +6,9 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Link,
+    useRouteMatch,
+    useParams
 } from 'react-router-dom';
 
 import {
@@ -15,8 +17,12 @@ import {
     Form
 } from 'react-bootstrap'
 
+import AddTodo from '../../containers/AddTodo';
+import VisibilityFilter from '../../containers/VisibleTodoList'
+
 const Navigation = () => {
     return (
+    <Router>
     <Navbar bg="light" expand="lg">
 
         <Navbar.Brand href="/home">Brand</Navbar.Brand>
@@ -28,17 +34,69 @@ const Navigation = () => {
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link href="/link1">link-1</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link href="/link-2">link-2</Nav.Link>
+                <Nav.Link href="/topics">Topics</Nav.Link>
             </Nav.Item>
         </Nav>
     </Navbar>
+    {
+        <Switch>
+            <Route path="/home">
+                <Home/>
+            </Route>
+            <Route path="/about">
+                <About/>
+            </Route>
+            <Route path="/topics">
+                <Topics/>
+            </Route>
+        </Switch>
+    }
+    </Router>
     );
 }
 
+function Home() {
+    return <h2>Home</h2>;
+}
+function About() {
+    return <h2>About</h2>;
+}
+function Topics() {
+    let {path, url} = useRouteMatch();
+    return(
+        <div>
+            <h2>Topics</h2>
+            <Nav>
+                <Nav.Item>
+                    <Nav.Link href={`${url}/components`}>Components</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href={`${url}/props-v-state`}>Props v. State</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            {
+                <Switch>
+                    <Route exact path={path}>
+                        <h3>Please select a topic.</h3>
+                    </Route>
+                    <Route path={`${path}/:topicId`}>
+                        <Topic />
+                    </Route>
+                </Switch>
+            }
+        </div>
+    );
+}
+
+function Topic() {
+    let { topicId } = useParams();
+    console.log("Topic: " + topicId);
+    return <h3>Request topic ID: {topicId}</h3>;
+}
 
 export default Navigation;
 
